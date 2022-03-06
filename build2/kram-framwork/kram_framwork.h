c47c7bb98b4be8b241157f18087f9cd8aea9ed39
Yaniv <yanivams@umake.xyz>
2022-03-06 23:57:24 +0200

    adding framwork target

---
+++ build2/kram-framwork/kram_framwork.h
@@ -0,0 +1,18 @@
+//
+//  kram_framwork.h
+//  kram-framwork
+//
+//  Created by Yaniv on 3/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kram_framwork.
+FOUNDATION_EXPORT double kram_framworkVersionNumber;
+
+//! Project version string for kram_framwork.
+FOUNDATION_EXPORT const unsigned char kram_framworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kram_framwork/PublicHeader.h>
+
+
